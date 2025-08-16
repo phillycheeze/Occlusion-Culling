5518db1609ab0b3e721f7d612d86fdabbc313975
Phil Prescher <12preschph@gmail.com>
2025-08-16 18:02:24 -0500

    Target patching of lod distance calculation

--- PreCullingSystemPatches.cs
+++ PreCullingSystemPatches.cs
@@ -20,4 +20,41 @@ namespace OcclusionCulling
             __result = __instance.EntityManager.CreateEntityQuery(desc);
         }
     }
+
+    [HarmonyPatch(typeof(PreCullingSystem), nameof(PreCullingSystem.OnUpdate))]
+    static class PreCullingLODScalePatch
+    {
+        static readonly MethodInfo Orig = AccessTools.Method(
+            typeof(RenderingUtils),
+            nameof(RenderingUtils.CalculateLodParameters),
+            new[] { typeof(float), typeof(LODParameters) }
+        );
+
+        static readonly MethodInfo Replacement = AccessTools.Method(
+            typeof(PreCullingLODScalePatch),
+            nameof(ScaleLodParameters)
+        );
+
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instr in instructions)
+            {
+                if (instr.opcode == OpCodes.Call && instr.operand as MethodInfo == Orig)
+                    instr.operand = Replacement;
+                yield return instr;
+            }
+        }
+
+        public static float4 ScaleLodParameters(float lodFactor, LODParameters p)
+        {
+            float4 baseParams = RenderingUtils.CalculateLodParameters(lodFactor, p);
+            // pull scale (1.0 = default)
+            float userScale = 0.5f; //Put behind user setting
+            // shorten cull-distance by dividing x, then recompute y = 1/(x*x)
+            baseParams.x /= userScale;
+            baseParams.y = 1f / (baseParams.x * baseParams.x);
+            return baseParams;
+        }
+    }
 }
